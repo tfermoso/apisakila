c19ef995b476482345f088292d60bad5325ab46d
tfermoso <tomas.fermoso@gmail.com>
2025-02-12 17:17:48 +0100

    endpoint new actor

--- actores/new.php
+++ actores/new.php
@@ -2,11 +2,17 @@
 if(isset($_POST["name"]) && $_POST["lastname"]){
 
 include("../conexiondb.php");
-$sql="select actor_id,first_name,last_name from actor where first_name=:first_name";
+$sql="insert into actor(first_name,last_name) values(:first_name,:last_name)";
 $stm=$conexion->prepare($sql);
-$stm->bindParam(":first_name",$_GET['name']);
+$stm->bindParam(":first_name",$_POST['name']);
+$stm->bindParam(':last_name',$_POST['lastname']);
 $stm->execute();
-$datos=$stm->fetchAll(PDO::FETCH_ASSOC);
+$actor_id=$conexion->lastInsertId();
+$datos=[
+    'actor_id'=>$actor_id,
+    'first_name'=>$_POST['name'],
+    'last_name'=>$_POST['lastname']
+];
 $json=json_encode($datos);
 // Establece la cabecera para indicar que la respuesta es JSON
 
