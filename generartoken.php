5a28de367546afd04c6bccd2bcd5755ed7ab1ed8
tfermoso <tomas.fermoso@gmail.com>
2025-02-13 19:01:00 +0100

    finalizamos login api

--- generartoken.php
+++ generartoken.php
@@ -1,8 +1,21 @@
 <?php
+require 'vendor/autoload.php';
+require 'config.php';
 use Firebase\JWT\JWT;
 
 if(isset($_POST["user"]) && isset($_POST["password"])) {
     $user = $_POST["user"];
     $password = $_POST["password"];
     //Consulta a la base de datos para verificar si el usuario y contraseña son correctos
+    //Si el usuario y contraseña son correctos, se genera un token
+   if($user=="admin" && $password=="admin") {
+        $token = generarToken(1);
+        echo json_encode(["token" => $token]);
+    } else {
+        http_response_code(401);
+        echo json_encode(["error" => "Usuario o contraseña incorrectos"]);
+    }
+}else {
+    http_response_code(400);
+    echo json_encode(["error" => "Faltan parámetros"]);
 }
