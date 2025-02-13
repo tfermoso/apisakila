f67357b6b474b6606103cb0e45d90820edd7ec37
tfermoso <tomas.fermoso@gmail.com>
2025-02-13 19:40:29 +0100

    verificar acceso

---
+++ validarAcceso.php
@@ -0,0 +1,18 @@
+require 'verificar_JWT.php';
+// Extraer el token desde la cabecera Authorization
+$headers = apache_request_headers();
+
+if (!isset($headers['Authorization'])) {
+    http_response_code(401);
+    echo json_encode(["error" => "Token no proporcionado"]);
+    exit();
+}
+
+$token = str_replace("Bearer ", "", $headers['Authorization']);
+$datos_usuario = verificarToken($token);
+
+if($datos_usuario["user_id"] != 1) {
+    http_response_code(403);
+    echo json_encode(["error" => "Acceso denegado"]);
+    exit();
+}
